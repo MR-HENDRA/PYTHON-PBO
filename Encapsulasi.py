cbad2c4ccfd2e89c244c37653779395075111f94
MR-HENDRA <hunterblackshadow26@gmail.com>
2022-03-04 21:39:22 +0800

    Encapsulasi

---
+++ Encapsulasi.py
@@ -0,0 +1,30 @@
+class Hero:
+    
+    def __init__(self,name,health,attackPower):
+        self.__name = name
+        self.__health = health
+        self.__attackPower = attackPower
+        
+    # getter
+    def getName(self):
+        return self.__name
+    
+    def getHealth(self):
+        return self.__health
+    
+    # setter
+    def diserang(self,serangPower):
+        self.__health -= serangPower
+        
+    def setAttPower(self,nilaibaru):
+        self.__attackPower = nilaibaru
+        
+# awal dari game
+Endangk = Hero("Endangk",100,1)
+
+# game berjalan
+
+print(Endangk.getName())
+print(Endangk.getHealth())
+Endangk.diserang(10)
+print(Endangk.getHealth())
